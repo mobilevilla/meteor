e8f2d176725749f1bb13c55248df5e6fbd972487
Łukasz Fechner <administrator@administrators-MacBook-Pro.local>
2022-03-12 15:52:46 +0100

    update

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-1_view01_view01",
-      "name": "1_View01_View01",
+      "id": "0-salon-kuchnia--living-room-kitchen",
+      "name": "Salon-Kuchnia  (Living room-Kitchen)",
       "levels": [
         {
           "tileSize": 256,
@@ -12,9 +12,17 @@ var APP_DATA = {
         {
           "tileSize": 512,
           "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
         }
       ],
-      "faceSize": 500,
+      "faceSize": 1500,
       "initialViewParameters": {
         "pitch": 0,
         "yaw": 0,
@@ -22,17 +30,17 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 3.01770874911659,
-          "pitch": 0.08691338683357586,
+          "yaw": 2.5805275255631734,
+          "pitch": 0.25898636811909626,
           "rotation": 0,
-          "target": "1-1_view01_view02"
+          "target": "1-toaleta-toilet"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "1-1_view01_view02",
-      "name": "1_View01_View02",
+      "id": "1-toaleta-toilet",
+      "name": "Toaleta (Toilet)",
       "levels": [
         {
           "tileSize": 256,
@@ -42,9 +50,17 @@ var APP_DATA = {
         {
           "tileSize": 512,
           "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
         }
       ],
-      "faceSize": 500,
+      "faceSize": 1500,
       "initialViewParameters": {
         "pitch": 0,
         "yaw": 0,
@@ -52,10 +68,10 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.0004010330997274991,
-          "pitch": 0,
+          "yaw": 0.01559128098605278,
+          "pitch": 0.2788383777555694,
           "rotation": 0,
-          "target": "0-1_view01_view01"
+          "target": "0-salon-kuchnia--living-room-kitchen"
         }
       ],
       "infoHotspots": []
